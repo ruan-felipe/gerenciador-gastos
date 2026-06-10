e8dd27a055d9888d0f1522381501bf411559aefc
ruan-felipe <43758052+ruan-felipe@users.noreply.github.com>
2026-06-10 19:08:47 -0300

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+# Módulo de utilitários
+def limpar_valor_csv(valor_str):
+    """Converte valores monetários do CSV (ex: 'R$ 45,67') para float."""
+    # Ex: transformar em 45.67
+    pass
+
+def formatar_data(data_str):
+    """Padroniza datas para o formato do banco de dados."""
+    pass
